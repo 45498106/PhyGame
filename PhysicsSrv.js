ec6c55e31528f9454aeae272681280aee679bed1
lishihai <jpweiyi@163.com>
2016-12-13 15:42:02 +0800

    1

---
+++ PhysicsSrv.js
@@ -0,0 +1,142 @@
+/*
+    server websocket framework.
+*/
+
+var express = require('express');
+var app = express();
+var AV = require('leanengine');
+var http = require('http').Server(app);
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'rH2mPgbjWGEf228we1rNQxMw-9Nh9j0Va',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'yikuJUITOfqB1lIqm3zPfom3',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'hAC6X9v0Ya7bvsM46NNLKLfE'
+});
+
+app.use(AV.express());
+app.use(express.static(__dirname + "/client"));
+
+http.listen( process.env.LEANCLOUD_APP_PORT, function() {
+    console.log('[DEBUG] Listening on *:' + process.env.LEANCLOUD_APP_PORT);
+});
+
+Util = require("./common/Utility.js");
+GameLog = require('./common/Logger.js');
+
+var phyGame = new (require('./server/PhysicsGame.js'))();
+phyGame.Init();
+
+//--------------------------------------------------
+// 常量
+//--------------------------------------------------
+var c_HeartbeatCheckMS = 1000;          //心跳检测毫秒数
+var c_HeartbeatCheckTimeoutCount = 2;   //心跳检测超时数量
+
+//--------------------------------------------------
+// 全局变量
+//--------------------------------------------------
+var clients = [];
+
+IO.on('connection', function (socket) {
+    GameLog('Client [' + socket.id + '] connected!');
+    
+    // 创建一个客户链接信息.
+    var client = { 
+                    id: socket.id, 
+                    socket : socket,
+                    timeoutCount : 0,
+                    heartbeatTime : new Date().getTime(), 
+                    SetHeartbeatTime : function() { this.heartbeatTime = new Date().getTime() },
+                };
+
+    clients.push(client);
+    
+    IO.emit('clientJoin', { name: client.id });
+    
+    GameLog('Total client: ' + clients.length);
+    
+    // 心跳响应
+    socket.on('heartbeat', function () {
+        client.SetHeartbeatTime();
+        socket.emit('heartbeatBack');
+    });
+    
+    // 断开链接
+    socket.on('disconnect', function () {
+        GameLog('Client [' + client.id + '] disconnected!');
+
+        // 通知phyGame 删除client
+        phyGame.DeleteClient(client);
+        
+        client.socket.broadcast.emit('clientDisconnect', { name: client.id  });
+            
+        var idx = Util.FindIndex(clients, client.id);
+        if ( idx >= 0 ) {
+            clients.splice(idx, 1);
+        }
+            
+        GameLog('Total players: ' + clients.length);
+    });
+    
+    // 通知phyGame 进入新client
+    phyGame.NewClient(client);
+});
+
+// 检测客户心跳
+function CheckClientHeartbeat()
+{
+    var now = new Date().getTime();
+    
+    for (var i = clients.length - 1; i >=0; --i) {
+        var client = clients[i];
+        if (now - client.heartbeatTime > c_HeartbeatCheckMS) {
+            ++client.timeoutCount;
+            if (client.timeoutCount > c_HeartbeatCheckTimeoutCount) {
+                // 超过超时次数,断开客户链接
+                client.socket.disconnect();
+            }
+        }else {
+            client.timeoutCount = 0;
+        }
+    }
+}
+
+setInterval(CheckClientHeartbeat, c_HeartbeatCheckMS);
+
+/*
+
+//--------------------------------------------------
+// client websocket framework :
+//--------------------------------------------------
+
+    var heartbeatTime = new Date().getTime();
+    var heartbeatHandler = null;
+    var socket = io('http://192.168.1.222');
+        
+    socket.on('clientJoin', function(data) {
+        console.log(data.name + " client 加入.");
+    });
+        
+    socket.on('clientDisconnect', function(data) {
+        console.log(data.name + " client 离开.");
+    });
+    
+    socket.on('heartbeatBack', function() {
+        var interval = new Date().getTime() - heartbeatTime;
+        console.log(interval);
+    });
+    
+    socket.on('disconnect', function() {
+        console.log("你已经断开链接.");
+        if (heartbeatHandler) {
+            clearInterval(heartbeatHandler);
+            heartbeatHandler = null;
+        }
+    });
+    
+    heartbeatHandler = setInterval(function() {
+        heartbeatTime = new Date().getTime();
+        socket.emit('heartbeat');
+    }, 1000);
+
+*/
---
+++ server/PhysicsGame.js
@@ -0,0 +1,205 @@
+if(typeof module !== 'undefined')
+    module.exports = PhysicsGame;
+
+var Box2D = require('./Box2dWeb-2.1.a.3.js');
+
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+    b2Math = Box2D.Common.Math.b2Math,
+    b2AABB = Box2D.Collision.b2AABB,
+    b2BodyDef = Box2D.Dynamics.b2BodyDef,
+    b2Body = Box2D.Dynamics.b2Body,
+    b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+    b2Fixture = Box2D.Dynamics.b2Fixture,
+    b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef,
+    b2PrismaticJointDef = Box2D.Dynamics.Joints.b2PrismaticJointDef,
+    b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef,
+    b2World = Box2D.Dynamics.b2World,
+    b2MassData = Box2D.Collision.Shapes.b2MassData,
+    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+
+var WorldWidht = 320, WorldHeight = 480;
+var BodyWidth = 40, BodyHeight = 40;
+            
+function CreataStaticBox(game,w,h,x,y) {
+    var fixDef = new b2FixtureDef;
+    fixDef.restitution = 0.0;
+    fixDef.shape = new b2PolygonShape;
+    fixDef.shape.SetAsBox(w/2*game.physics.gameScale, h/2*game.physics.gameScale);
+        
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_staticBody; //默认是静态的
+    bodyDef.position.x = x * game.physics.gameScale;
+    bodyDef.position.y = y * game.physics.gameScale;
+    game.physics.world.CreateBody(bodyDef).CreateFixture(fixDef);
+}
+
+function CreateDynamicBox(game,w,h,x,y,angle) {
+    var fixDef = new b2FixtureDef;
+    fixDef.density=0.1;
+    fixDef.restitution=1.0;
+    fixDef.shape = new b2PolygonShape;
+    fixDef.shape.SetAsBox(
+        w/2*game.physics.gameScale, //half width
+        h/2*game.physics.gameScale //half height
+    );
+    
+    fixDef.shape.SetAsBox(w/2*game.physics.gameScale, h/2*game.physics.gameScale);
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_dynamicBody;
+    bodyDef.angle = Util.Degrees(angle);
+    bodyDef.linearDamping=0.0;
+    bodyDef.angularDamping=0.0;
+    bodyDef.position.x = x * game.physics.gameScale;
+    bodyDef.position.y = y * game.physics.gameScale;
+    var body = game.physics.world.CreateBody(bodyDef);
+    body.CreateFixture(fixDef);
+    return body;
+}
+
+
+function getBodyAtMouse(game, mouseX, mouseY) {
+    var mousePVec = new b2Vec2(mouseX, mouseY);
+    var aabb = new b2AABB();
+    aabb.lowerBound.Set(mouseX - 0.001, mouseY - 0.001);
+    aabb.upperBound.Set(mouseX + 0.001, mouseY + 0.001);
+    
+    function getBodyCB(fixture) {
+        if(fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+           if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePVec)) {
+              selectedBody = fixture.GetBody();
+              return false;
+           }
+        }
+        return true;
+    }
+
+    // Query the world for overlapping shapes.
+    selectedBody = null;
+    game.physics.world.QueryAABB(getBodyCB, aabb);
+    return selectedBody;
+}
+
+        
+function PhysicsGame()
+{
+
+}
+
+PhysicsGame.prototype.Init = function()
+{
+    this.players = [];
+
+    this.physics = {}
+    this.physics.world = new b2World(
+        new b2Vec2(0, -9.8),    //gravity
+        true                 //allow sleep
+    );
+    this.physics.gameScale = (1 / 10);
+    this.physics.gameInvScale = 10;
+    this.mouseJoint = null;
+    
+    // 创建包围墙
+    CreataStaticBox(this, WorldWidht, 10, WorldWidht/2, 5);
+    CreataStaticBox(this, 10, WorldHeight, 5, WorldHeight/2);
+    CreataStaticBox(this, WorldWidht, 10, WorldWidht/2, WorldHeight-5);
+    CreataStaticBox(this, 10, WorldHeight, WorldWidht-5, WorldHeight/2);
+    
+    this.body = CreateDynamicBox(this, BodyWidth, BodyHeight, WorldWidht/2, WorldHeight/2, Math.random() * 180);
+
+    // 设置更新
+    setInterval(function(){
+        this._pdt = (new Date().getTime() - this._pdte) / 1000.0;
+        this._pdte = new Date().getTime();
+        if (this._pdt > 0.04) {
+            GameLog("!!!###########this._pdt=", this._pdt);
+        }
+        this.Update(this._pdt);
+    }.bind(this), 1000.0/30.0);
+}
+
+PhysicsGame.prototype.Update = function(dt)
+{
+    //
+    //console.log(p.x, p.y);
+
+    this.physics.world.Step(
+               dt,      //frame-rate
+               2,      //velocity iterations
+               4       //position iterations
+    );
+    
+    var p = this.body.GetPosition();
+    var a = this.body.GetAngle();
+    var player;
+    for (var i = 0; i < this.players.length; ++i) {
+        player = this.players[i];
+        player.socket.emit('physicsSynchro', { 'body_x' : p.x * this.physics.gameInvScale, 'body_y' : p.y * this.physics.gameInvScale, 'body_angle' : a});
+    }
+    
+    // 放在最后
+    this.physics.world.ClearForces();
+}
+
+
+PhysicsGame.prototype.NewClient = function(client)
+{
+    var socket = client.socket;
+    var server = this;
+    
+    socket.on('enterGame', function (data) {
+        
+        if (typeof client.player !== 'undefined') {
+            GameLog(socket.id + "repeat enterGame");
+            return;
+        }
+        
+        var newPlayer = { 'socket' : socket  }
+        client.player = newPlayer; 
+        server.players.push(newPlayer);
+        
+        var data = { 'id' :  socket.id, 'body_width' : BodyWidth, 'body_height' : BodyHeight };
+        socket.emit('enterGameBack', data);
+    });
+    
+    socket.on('input', function (data) {
+        if(data.state === 1 && (!client.player.mouseJoint)) {
+            var mx = data.x * server.physics.gameScale;
+            var my = data.y * server.physics.gameScale;
+            var body = getBodyAtMouse(server, mx, my);
+            if(body) {
+                var md = new b2MouseJointDef();
+                md.bodyA = server.physics.world.GetGroundBody();
+                md.bodyB = body;
+                md.target.Set(mx, my);
+                md.collideConnected = true;
+                md.maxForce = 300.0 * body.GetMass();
+                client.player.mouseJoint = server.physics.world.CreateJoint(md);
+                body.SetAwake(true);
+            }
+        }
+        
+        if(client.player.mouseJoint) {
+           if(data.state === 1) {
+                var mx = data.x * server.physics.gameScale;
+                var my = data.y * server.physics.gameScale;
+                client.player.mouseJoint.SetTarget(new b2Vec2(mx, my));
+           } else {
+              server.physics.world.DestroyJoint(client.player.mouseJoint);
+              client.player.mouseJoint = null;
+           }
+        }
+    });
+}
+
+PhysicsGame.prototype.DeleteClient = function(client)
+{
+    var player = client.player;
+    if (player) {
+        var idx = Util.FindIndex(this.players, player.id);
+        if ( idx >= 0 ) {
+            this.players.splice(idx, 1);
+        }
+        client.player = null;
+    }
+}
